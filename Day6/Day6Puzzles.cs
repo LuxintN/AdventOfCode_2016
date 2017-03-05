e528ce9a91edcac4907afb5ea6c3db307d864ff4
Anastasiia Nevidomska <anastasiia.nevidomska@gmail.com>
2017-03-05 19:50:29 +0200

    Add Day 6 puzzles with unit tests

---
+++ Day6/Day6Puzzles.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    public class Day6Puzzles
+    {
+        static void Main(string[] args)
+        {
+            var input = LoadDataFromInputFile();
+
+            Console.WriteLine(GetMessage(input, true));
+            Console.WriteLine(GetMessage(input, false));
+
+            Console.ReadLine();
+        }
+
+        public static string GetMessage(string input, bool selectMostCommonCharacter)
+        {
+            var columns = SplitIntoColumns(input);
+
+            if (columns.Count == 0 || columns.Any(c => c.Length != columns.First().Length)) 
+                throw new ArgumentException("Column list is empty or columns have unequal lengths");
+
+            var message = string.Concat(
+                columns.Select(c => c.ToList())
+                    .Select(
+                        c => 
+                            selectMostCommonCharacter
+                                ? c.GroupBy(ch => ch).OrderByDescending(g => g.Count()).First().Key
+                                : c.GroupBy(ch => ch).OrderByDescending(g => g.Count()).Last().Key));
+            
+            return message;
+        }
+
+        private static List<string> SplitIntoColumns(string input)
+        {
+            var columns = new List<string>();
+
+            using (var stringReader = new StringReader(input))
+            {
+                string row;
+
+                while ((row = stringReader.ReadLine()) != null)
+                {
+                    row = row.Trim();
+
+                    if (columns.Count < row.Length)
+                    {
+                        var lengthDifference = row.Length - columns.Count;
+                        for (int i = 0; i < lengthDifference; i++)
+                        {
+                            columns.Add(string.Empty);
+                        }
+                    }
+
+                    for (int i = 0; i < row.Length; i++)
+                    {
+                        columns[i] += row[i];
+                    }
+                }
+            }
+
+            return columns;
+        }
+
+        private static string LoadDataFromInputFile()
+        {
+            using (var streamReader = new StreamReader(Environment.CurrentDirectory + @"\..\..\Input.txt"))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ UnitTests/Day6Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Day6;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class Day6Tests
+    {
+        private const string inputFromExample =  @" eedadn
+                                                    drvtee
+                                                    eandsr
+                                                    raavrd
+                                                    atevrs
+                                                    tsrnev
+                                                    sdttsa
+                                                    rasrtv
+                                                    nssdts
+                                                    ntnada
+                                                    svetve
+                                                    tesnvt
+                                                    vntsnd
+                                                    vrdear
+                                                    dvrsen
+                                                    enarar";
+        [Test]
+        [TestCase("abc","abc")]
+        [TestCase("abc \n ade \n bdc", "adc")]
+        [TestCase(inputFromExample, "easter")]
+        public void GetMessage_WhenConfiguredForMostCommonCharacters_MessageConsistsOfTheMostCommonCharactersInEachColumn(string input, string expectedResult)
+        {
+            var message = Day6Puzzles.GetMessage(input, true);
+            Assert.AreEqual(expectedResult, message);
+        }
+
+        [Test]
+        [TestCase("abc", "abc")]
+        [TestCase("abc \n ade \n bdc", "bbe")]
+        [TestCase(inputFromExample, "advent")]
+        public void GetMessage_WhenConfiguredForLeastCommonCharacters_MessageConsistsOfTheLeastCommonCharactersInEachColumn(string input, string expectedResult)
+        {
+            var message = Day6Puzzles.GetMessage(input, false);
+            Assert.AreEqual(expectedResult, message);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("abc\nde")]
+        public void GetMessage_WhenInputFormatIsInvalid_ThrowsArgumentException(string input)
+        {
+            Assert.Throws<ArgumentException>(() => Day6Puzzles.GetMessage(input, true));
+        }   
+    }
+}
