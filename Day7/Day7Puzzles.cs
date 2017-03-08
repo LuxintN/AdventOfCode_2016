d88b9583d823c965c17e35d69139a6ad12052266
Anastasiia Nevidomska <anastasiia.nevidomska@gmail.com>
2017-03-08 16:37:07 +0200

    Add Day 7 puzzles with unit tests

---
+++ Day7/Day7Puzzles.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day7
+{
+    public class Day7Puzzles
+    {
+        private static readonly Regex insideSquareBracketsRegex = new Regex(@"\[\w+\]");
+        
+        static void Main(string[] args)
+        {
+            var addresses = LoadDataFromInputFile();
+
+            var addressesSupportingTlsCount = addresses.Count(CheckIfSupportsTls);
+            Console.WriteLine(addressesSupportingTlsCount);
+
+            var addressesSupportingSslCount = addresses.Count(CheckIfSupportsSsl);
+            Console.WriteLine(addressesSupportingSslCount);
+
+            Console.ReadLine();
+        }
+        
+        public static bool CheckIfSupportsTls(string input)
+        {
+            return GetValuesOutsideSquareBrackets(input).Any(ContainsABBA) && GetValuesInsideSquareBrackets(input).All(x => !ContainsABBA(x));
+        }
+
+        public static bool CheckIfSupportsSsl(string input)
+        {
+            var abaValues = GetValuesOutsideSquareBrackets(input).SelectMany(GetABAValues).ToList();
+            
+            return abaValues.Count > 0 && GetValuesInsideSquareBrackets(input).Any(x => ContainsBAB(x, abaValues));
+        }
+
+        private static bool ContainsABBA(string input)
+        {
+            for (int i = 0; i < input.Length-3; i++)
+            {
+                var substring = input.Substring(i, 4);
+
+                if (substring[0] == substring[3] && substring[1] == substring[2] && substring[0] != substring[1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static IEnumerable<string> GetABAValues(string input)
+        {
+            var abaValues = new HashSet<string>();
+
+            for (int i = 0; i < input.Length - 2; i++)
+            {
+                var substring = input.Substring(i, 3);
+
+                if (substring[0] == substring[2] && substring[0] != substring[1])
+                {
+                    abaValues.Add(substring);
+                }
+            }
+
+            return abaValues;
+        }
+
+        private static bool ContainsBAB(string input, List<string> abaValues)
+        {
+            foreach (var abaValue in abaValues)
+            {
+                for (int i = 0; i < input.Length - 2; i++)
+                {
+                    var substring = input.Substring(i, 3);
+
+                    if (substring[0] == abaValue[1] && substring[1] == abaValue[0] && substring[2] == abaValue[1])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        
+        private static IEnumerable<string> GetValuesOutsideSquareBrackets(string input)
+        {
+            return insideSquareBracketsRegex.Split(input);
+        }
+
+        private static IEnumerable<string> GetValuesInsideSquareBrackets(string input)
+        {
+            return insideSquareBracketsRegex.Matches(input).Cast<Match>().Select(m => m.Value.Substring(1, m.Value.Length - 2));
+        }
+
+        private static List<string> LoadDataFromInputFile()
+        {
+            var lines = new List<string>();
+
+            using (var streamReader = new StreamReader(Environment.CurrentDirectory + @"\..\..\Input.txt"))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    lines.Add(streamReader.ReadLine());
+                }
+            }
+
+            return lines;
+        }
+
+    }
+}
---
+++ UnitTests/Day7Tests.cs
@@ -0,0 +1,30 @@
+﻿using Day7;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class Day7Tests
+    {
+        [Test]
+        [TestCase("abba[mnop]qrst",true)]
+        [TestCase("abcd[bddb]xyyx", false)]
+        [TestCase("aaaa[qwer]tyui", false)]
+        [TestCase("ioxxoj[asdfgh]zxcvbn", true)]
+        public void CheckIfSupportsTls_ReturnsCorrectValue(string input, bool expectedValue)
+        {
+            bool result = Day7Puzzles.CheckIfSupportsTls(input);
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [Test]
+        [TestCase("aba[bab]xyz", true)]
+        [TestCase("xyx[xyx]xyx", false)]
+        [TestCase("aaa[kek]eke", true)]
+        [TestCase("zazbz[bzb]cdb", true)]
+        public void CheckIfSupportsSsl_ReturnsCorrectValue(string input, bool expectedValue)
+        {
+            bool result = Day7Puzzles.CheckIfSupportsSsl(input);
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
