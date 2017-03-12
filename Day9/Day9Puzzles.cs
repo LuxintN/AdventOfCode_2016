8e645a48e917bb85aa8591fd2075010fce0a3a51
Anastasiia Nevidomska <anastasiia.nevidomska@gmail.com>
2017-03-12 15:22:05 +0200

    Add Day 9 puzzles with unit tests

---
+++ Day9/Day9Puzzles.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day9
+{
+    public class Day9Puzzles
+    {   
+        static void Main(string[] args)
+        {
+            var input = LoadDataFromInputFile();
+
+            string decompressedString;
+
+            long decompressedStringLength = GetDecompressedStringLength(input, false, out decompressedString);
+            Console.WriteLine(decompressedString);
+            Console.WriteLine(decompressedStringLength);
+
+            decompressedStringLength = GetDecompressedStringLength(input, true, out decompressedString);
+            Console.WriteLine(decompressedString);
+            Console.WriteLine(decompressedStringLength);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Calculates the length of the decompressed string
+        /// </summary>
+        /// <param name="input">The input string to decompress</param>
+        /// <param name="isRecursionEnabled">When true, goes through all markers recursively to calculate the decompressed string length, 
+        /// otherwise treats the markers within the repeat sequence of other markers as regular text</param>
+        /// <param name="decompressedString">The decomressed string. NOTE: always null if <paramref name="isRecursionEnabled"/> is set to true 
+        /// as the result string may be too long to process</param>
+        /// <returns>The length of the decompressed string</returns>
+        public static long GetDecompressedStringLength(string input, bool isRecursionEnabled, out string decompressedString)
+        {
+            long decompressedStringLength = 0; 
+            decompressedString = isRecursionEnabled ? null : string.Empty;
+
+            int i = 0;
+            bool isInsideMarker = false, isReadingSequenceLength = true, hasFinishedReadingMarker = false;
+            string sequenceLength = string.Empty, repeatCount = string.Empty;
+
+            while (i < input.Length)
+            {
+                if (!isInsideMarker)
+                {
+                    if (input[i] == '(')
+                    {
+                        isInsideMarker = true;
+                    }
+                    else
+                    {
+                        decompressedStringLength++;
+                        
+                        if (!isRecursionEnabled)
+                        {
+                            decompressedString += input[i];
+                        }
+                    }
+                }
+                else
+                {
+                    if (input[i] == 'x')
+                    {
+                        isReadingSequenceLength = false;
+                    }
+                    else if (input[i] == ')')
+                    {
+                        hasFinishedReadingMarker = true;
+                    }
+                    else if (isReadingSequenceLength)
+                    {
+                        sequenceLength += input[i];
+                    }
+                    else
+                    {
+                        repeatCount += input[i];
+                    }
+                }
+
+                i++;
+
+                if (hasFinishedReadingMarker)
+                {
+                    var sequenceToRepeat = input.Substring(i, int.Parse(sequenceLength));
+                    i += sequenceToRepeat.Length;
+
+                    if (isRecursionEnabled)
+                    {
+                        string decompressedSubstring;
+                        decompressedStringLength += long.Parse(repeatCount) *
+                                                    GetDecompressedStringLength(sequenceToRepeat, true, out decompressedSubstring);
+                    }
+                    else
+                    {
+                        var repeatedSequence = string.Concat(Enumerable.Repeat(sequenceToRepeat, int.Parse(repeatCount)));
+                        decompressedString += repeatedSequence;
+                        decompressedStringLength += repeatedSequence.Length;
+                    }
+
+                    sequenceLength = string.Empty;
+                    repeatCount = string.Empty;
+                    isInsideMarker = false;
+                    isReadingSequenceLength = true;
+                    hasFinishedReadingMarker = false;
+                }
+            }
+
+            return decompressedStringLength;
+        }
+
+        private static string LoadDataFromInputFile()
+        {
+            string input;
+
+            using (var streamReader = new StreamReader(Environment.CurrentDirectory + @"\..\..\Input.txt"))
+            {
+                input = streamReader.ReadToEnd();
+            }
+
+            return input;
+        }
+    }
+}
---
+++ UnitTests/Day9Tests.cs
@@ -0,0 +1,48 @@
+﻿using Day9;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class Day9Tests
+    {
+        [Test]
+        [TestCase("ADVENT", "ADVENT")]
+        [TestCase("A(1x5)BC", "ABBBBBC")]
+        [TestCase("(3x3)XYZ", "XYZXYZXYZ")]
+        [TestCase("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG")]
+        [TestCase("(6x1)(1x3)A", "(1x3)A")]
+        [TestCase("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY")]
+        public void GetDecompressedStringLength_RecursionIsDisabled_ReturnsCorrectLengthAndString(string input, string expectedDecompressedString)
+        {
+            // Arrange
+            string decompressedString;
+
+            // Act
+            var decompressedStringLength = Day9Puzzles.GetDecompressedStringLength(input, false, out decompressedString);
+
+            // Assert
+            Assert.AreEqual(expectedDecompressedString, decompressedString);
+            Assert.AreEqual(expectedDecompressedString.Length, decompressedStringLength);
+        }
+
+        [Test]
+        [TestCase("ADVENT", 6)]
+        [TestCase("(3x3)XYZ", 9)]
+        [TestCase("X(8x2)(3x3)ABCY", 1+2*(3*3)+1)]
+        [TestCase("(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920)]
+        [TestCase("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445)]
+
+        public void GetDecompressedStringLength_RecursionIsEnabled_ReturnsCorrectLength(string input, int expectedDecompressedStringLength)
+        {
+            // Arrange
+            string decompressedString;
+
+            // Act
+            var decompressedStringLength = Day9Puzzles.GetDecompressedStringLength(input, true, out decompressedString);
+
+            // Assert
+            Assert.AreEqual(decompressedString, null);
+            Assert.AreEqual(expectedDecompressedStringLength, decompressedStringLength);
+        }
+    }
+}
